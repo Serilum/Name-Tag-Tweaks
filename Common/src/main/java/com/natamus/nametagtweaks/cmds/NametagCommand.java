f75eee74fd2ea930c60102cae4c3f27607f8bc30
Rick South <rick@natamus.com>
2023-12-12 09:36:40 +0100

    Name Tag Tweaks source code for 1.20.4.

---
+++ Common/src/main/java/com/natamus/nametagtweaks/cmds/NametagCommand.java
@@ -0,0 +1,54 @@
+package com.natamus.nametagtweaks.cmds;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.natamus.collective.functions.StringFunctions;
+import com.natamus.nametagtweaks.config.ConfigHandler;
+
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.network.chat.Component;
+import net.minecraft.ChatFormatting;
+
+public class NametagCommand {
+	public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
+		dispatcher.register(Commands.literal("nametag")
+			.requires((iCommandSender) -> iCommandSender.getEntity() instanceof ServerPlayer)
+			.executes((command) -> {
+				StringFunctions.sendMessage(command.getSource(), "Usage: '/nametag <name>' while holding a name tag.", ChatFormatting.DARK_GREEN);
+				return 1;
+			})
+			.then(Commands.argument("name", StringArgumentType.word())
+			.executes((command) -> {
+				Player player = command.getSource().getPlayerOrException();
+				
+				ItemStack nametagstack;
+				if (player.getItemInHand(InteractionHand.MAIN_HAND).getItem().equals(Items.NAME_TAG)) {
+					nametagstack = player.getItemInHand(InteractionHand.MAIN_HAND);
+				}
+				else if (player.getItemInHand(InteractionHand.OFF_HAND).getItem().equals(Items.NAME_TAG)) {
+					nametagstack = player.getItemInHand(InteractionHand.OFF_HAND);
+				}
+				else {
+					StringFunctions.sendMessage(player, "Usage: '/nametag <name>' while holding a name tag.", ChatFormatting.RED);
+					return 1;
+				}
+				
+				String name = StringArgumentType.getString(command, "name");
+				if (ConfigHandler.nameTagCommandReplaceUnderscoresWithSpaces) {
+					name = name.replace("_", " ");
+				}
+				
+				nametagstack.setHoverName(Component.literal(name));
+				nametagstack.setRepairCost(0);
+				StringFunctions.sendMessage(player, "Set name value to '" + name + "'.", ChatFormatting.DARK_GREEN);
+				return 1;
+			}))
+		);
+	}
+}

76aa07e9f06920a838cecc60943d2af92d780b3e
Rick South <rick@natamus.com>
2024-04-28 15:53:37 +0200

    Name Tag Tweaks source code for 1.20.5.

---
+++ Common/src/main/java/com/natamus/nametagtweaks/events/NameTagEvent.java
@@ -0,0 +1,42 @@
+package com.natamus.nametagtweaks.events;
+
+import com.natamus.nametagtweaks.config.ConfigHandler;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.monster.Slime;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+
+public class NameTagEvent {
+	public static void mobItemDrop(Level level, Entity entity, DamageSource damageSource) {
+		if (level.isClientSide) {
+			return;
+		}
+		
+		if (entity.hasCustomName()) {
+			if (entity instanceof Slime) {
+				Slime slime = (Slime)entity;
+				int slimesize = slime.getSize();
+				if (slimesize != 4) {
+					return;
+				}
+			}
+			
+			ItemStack nametagstack = new ItemStack(Items.NAME_TAG, 1);
+			if (ConfigHandler.droppedNameTagbyEntityKeepsNameValue) {
+				Component name = entity.getName();
+				nametagstack.set(DataComponents.ITEM_NAME, name);
+			}
+			nametagstack.set(DataComponents.REPAIR_COST, 0);
+			
+			Vec3 evec = entity.position();
+			ItemEntity ie = new ItemEntity(level, evec.x(), evec.y()+1, evec.z(), nametagstack);
+			level.addFreshEntity(ie);
+		}
+	}
+}
---
+++ Forge/src/main/java/com/natamus/nametagtweaks/forge/events/ForgeNameTagEvent.java
@@ -0,0 +1,26 @@
+package com.natamus.nametagtweaks.forge.events;
+
+import com.natamus.nametagtweaks.cmds.NametagCommand;
+import com.natamus.nametagtweaks.config.ConfigHandler;
+import com.natamus.nametagtweaks.events.NameTagEvent;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeNameTagEvent {
+    @SubscribeEvent
+    public void registerCommands(RegisterCommandsEvent e) {
+    	if (ConfigHandler.enableNameTagCommand) {
+    		NametagCommand.register(e.getDispatcher());
+    	}
+    }
+
+	@SubscribeEvent
+	public void mobItemDrop(LivingDeathEvent e) {
+		LivingEntity livingEntity = e.getEntity();
+		NameTagEvent.mobItemDrop(livingEntity.level(), e.getEntity(), e.getSource());
+	}
+}

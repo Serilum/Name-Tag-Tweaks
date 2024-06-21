066671726eb3e9095b3b378e6ab96a9c03ad1161
Rick South <rick@natamus.com>
2024-06-21 11:34:00 +0200

    (v3.6) (1.21.0) Fixed recipe not being applied correctly.

--- Common/src/main/java/com/natamus/nametagtweaks/util/Reference.java
+++ Common/src/main/java/com/natamus/nametagtweaks/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.nametagtweaks.util;
 public class Reference {
 	public static final String MOD_ID = "nametagtweaks";
 	public static final String NAME = "Name Tag Tweaks";
-	public static final String VERSION = "3.5";
+	public static final String VERSION = "3.6";
 	public static final String ACCEPTED_VERSIONS = "[1.21.0]";
 }

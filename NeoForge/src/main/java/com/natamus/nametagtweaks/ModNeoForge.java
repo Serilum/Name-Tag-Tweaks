56def3df7346fbc262a2662d785a1e9eae1d9c7c
Rick South <rick@natamus.com>
2024-07-28 07:24:32 +0200

    (v3.7-3.8) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/nametagtweaks/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/nametagtweaks/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.nametagtweaks;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.nametagtweaks.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.nametagtweaks.neoforge.events.NeoForgeNameTagEvent;
 import com.natamus.nametagtweaks.util.Reference;
@@ -14,6 +15,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();

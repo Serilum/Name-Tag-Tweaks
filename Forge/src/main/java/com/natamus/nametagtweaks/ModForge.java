7e55581f0024306486cdc2b7ac9702c964d9b289
Rick South <rick@natamus.com>
2024-08-09 08:15:10 +0200

    Name Tag Tweaks source code for 1.21.1.

---
+++ Forge/src/main/java/com/natamus/nametagtweaks/ModForge.java
@@ -0,0 +1,41 @@
+package com.natamus.nametagtweaks;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.nametagtweaks.forge.config.IntegrateForgeConfig;
+import com.natamus.nametagtweaks.forge.events.ForgeNameTagEvent;
+import com.natamus.nametagtweaks.util.Reference;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateForgeConfig.registerScreen(ModLoadingContext.get());
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+    	MinecraftForge.EVENT_BUS.register(new ForgeNameTagEvent());
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}

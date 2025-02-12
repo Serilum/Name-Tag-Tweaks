1e35e14179a33519d865002834b254348fd3a65b
Rick South <rick@natamus.com>
2025-02-12 23:24:43 +0100

    (v4.0) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/nametagtweaks/ModForge.java
+++ Forge/src/main/java/com/natamus/nametagtweaks/ModForge.java
@@ -32,7 +32,7 @@ public class ModForge {
 	}
 
 	private void loadComplete(final FMLLoadCompleteEvent event) {
-    	MinecraftForge.EVENT_BUS.register(new ForgeNameTagEvent());
+    	MinecraftForge.EVENT_BUS.register(ForgeNameTagEvent.class);
 	}
 
 	private static void setGlobalConstants() {

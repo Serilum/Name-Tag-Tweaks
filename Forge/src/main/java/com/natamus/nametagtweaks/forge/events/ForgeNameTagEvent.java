3f0e440fbb97f44759be525b877c228bb68d8295
Rick South <rick@natamus.com>
2025-03-28 00:40:26 +0100

    Name Tag Tweaks source code for 1.21.5.

---
+++ Forge/src/main/java/com/natamus/nametagtweaks/forge/events/ForgeNameTagEvent.java
@@ -0,0 +1,24 @@
+package com.natamus.nametagtweaks.forge.events;
+
+import com.natamus.nametagtweaks.cmds.NametagCommand;
+import com.natamus.nametagtweaks.config.ConfigHandler;
+import com.natamus.nametagtweaks.events.NameTagEvent;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ForgeNameTagEvent {
+    @SubscribeEvent
+    public static void registerCommands(RegisterCommandsEvent e) {
+    	if (ConfigHandler.enableNameTagCommand) {
+    		NametagCommand.register(e.getDispatcher());
+    	}
+    }
+
+	@SubscribeEvent
+	public static void mobItemDrop(LivingDeathEvent e) {
+		LivingEntity livingEntity = e.getEntity();
+		NameTagEvent.mobItemDrop(livingEntity.level(), e.getEntity(), e.getSource());
+	}
+}
